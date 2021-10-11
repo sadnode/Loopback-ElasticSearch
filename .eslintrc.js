73966eac53988176ba4290ab219e5c2859905888
Igor <igor_csns_s2@hotmail.com>
2021-10-11 11:09:45 -0300

    extend eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
   extends: '@loopback/eslint-config',
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: [
+          'camelCase',
+          'strictCamelCase',
+          'PascalCase',
+          'StrictPascalCase',
+          'snake_case',
+          'UPPER_CASE',
+        ],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+    ],
+  }
 };
